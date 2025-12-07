ca3b1420311582fbd5e4c250e53a305a860c7026
DomiD94941 <dominik.darlak@outlook.com>
2025-12-07 16:51:07 +0100

    feat: add dropping sequence and tables for schema

--- database_scripts/01_schema.sql
+++ database_scripts/01_schema.sql
@@ -3,12 +3,11 @@ ALTER SESSION SET CURRENT_SCHEMA = TA_APP;
 
 BEGIN
   FOR s IN (
-    SELECT sequence_name 
-    FROM all_sequences
-    WHERE sequence_owner = UPPER('&TARGET_SCHEMA')
-  ) LOOP
+      SELECT sequence_name FROM all_sequences WHERE sequence_owner = 'TA_APP'
+  )
+  LOOP
     BEGIN
-      EXECUTE IMMEDIATE 'DROP SEQUENCE &TARGET_SCHEMA.' || s.sequence_name;
+      EXECUTE IMMEDIATE 'DROP SEQUENCE TA_APP.' || s.sequence_name;
     EXCEPTION WHEN OTHERS THEN NULL;
     END;
   END LOOP;
@@ -16,19 +15,18 @@ END;
 /
 
 BEGIN
-    FOR t IN (
-        SELECT table_name
-        FROM all_tables
-        WHERE owner = UPPER('&TARGET_SCHEMA')
-    ) LOOP
-        BEGIN
-            EXECUTE IMMEDIATE
-                'DROP TABLE ' || '&TARGET_SCHEMA' || '.' || t.table_name || ' CASCADE CONSTRAINTS';
-        EXCEPTION WHEN OTHERS THEN NULL;
-        END;
-    END LOOP;
+  FOR t IN (
+      SELECT table_name FROM all_tables WHERE owner = 'TA_APP'
+  )
+  LOOP
+    BEGIN
+      EXECUTE IMMEDIATE 'DROP TABLE TA_APP.' || t.table_name || ' CASCADE CONSTRAINTS';
+    EXCEPTION WHEN OTHERS THEN NULL;
+    END;
+  END LOOP;
 END;
 /
+
 
 CREATE SEQUENCE seq_clients START WITH 1 INCREMENT BY 1 NOCACHE;
 CREATE SEQUENCE seq_guides START WITH 1 INCREMENT BY 1 NOCACHE;
